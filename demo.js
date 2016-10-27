3401d5a02231bcbe2de174bd2aca5b344c2f8cc4
huhuimin <huhuimin@zbj.com>
2016-10-27 18:20:22 +0800

    2016-10-27学习笔记

--- demo.js
+++ demo.js
@@ -1,24 +1,34 @@
 /**
  * Created by hhm on 2016/10/11.
  */
-function displayInfo(arg){
-    var output = '';
-    if (typeof arg.name == 'string'){
-        output += 'name:'+arg.name+'\n';
-    }
-    if (typeof arg.age == 'number'){
-        output += 'age:'+arg.age;
-    }
-   console.log(output);
-}
-displayInfo({
-    "name":"hh",
-    'age':12
-});
-displayInfo({
-    "name":"hhd",
-    'age':17
-});
+//function displayInfo(arg){
+//    var output = '';
+//    if (typeof arg.name == 'string'){
+//        output += 'name:'+arg.name+'\n';
+//    }
+//    if (typeof arg.age == 'number'){
+//        output += 'age:'+arg.age;
+//    }
+//   console.log(output);
+//}
+//displayInfo({
+//    "name":"hh",
+//    'age':12
+//});
+//displayInfo({
+//    "name":"hhd",
+//    'age':17
+//});
+//
+//var personName = 'dd';
+//alert(person[personName]);
 
-var personName = 'dd';
-alert(person[personName]);
+$(document).ready(function(){
+    $('.box2').each(function(){
+        var maxWidth = 23;
+        if($('.box2').text().length>maxWidth){
+            $(this).text($(this).text().substring(0,maxWidth));
+            $(this).html($(this).html()+'...');
+        }
+    });
+});
